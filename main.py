c152296428fc7432ed0f1513bcb8f71f4ea17bec
Thanusha Panaluwa <thanushapanaluwa@gmail.com>
2022-09-14 11:40:25 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+#just for fun. I sent this message to my friend as a executable file...
 import time
 print("hello Helani! This is a warning!!!! Becareful Darling....")
-print("This is a scam . I am sending you a virus with this. Becarefullllll")
+print("This is me. I am sending you a message with this. Look!")
 
 time.sleep(4)
 
