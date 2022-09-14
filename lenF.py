95fcb70eb98cd694fbd708e1fcded8af880f0ce8
Thanusha Panaluwa <thanushapanaluwa@gmail.com>
2022-09-14 12:24:06 +0800

    Add files via upload
    Use len() function with index (Get one letter from string)

---
+++ lenF.py
@@ -0,0 +1,10 @@
+import time
+
+a = input("Enter your input to be count:")
+b = len(a)
+c = str(b)
+d = print("There are" + " " + c + " " + "number of elements.")
+e = str(d)[3]
+print(e)
+
+time.sleep(5)
