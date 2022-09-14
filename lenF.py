ef2fa2b42152c753fa84ec6ee120fd7f21ee551e
Thanusha Panaluwa <thanushapanaluwa@gmail.com>
2022-09-14 12:25:50 +0800

    Update lenF.py

--- lenF.py
+++ lenF.py
@@ -4,7 +4,7 @@ a = input("Enter your input to be count:")
 b = len(a)
 c = str(b)
 d = print("There are" + " " + c + " " + "number of elements.")
-e = str(d)[3]
+e = str(d)[3] # Get the third element from the text
 print(e)
 
 time.sleep(5)
